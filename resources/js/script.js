df67c492f88070d1996f7e41dd6daa54dfe974c1
A-Maged <a.maged99@outlook.com>
2018-05-22 06:24:18 +0200

    .

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,13 +2,12 @@
 
 
 // smooth scroll
-$(document).on('click', '.scroll', function(e){
+$('.nav .scroll').on('click', function(e){
 	e.preventDefault();
-	var body = $('body');	// cache for performance
-	var x = ($( $.attr(this, 'href') ).offset().top) - 51;
-	body.animate({
-		scrollTop: x
-	}, 900);
+	var body = $('html,body');
+	var position = ( $($.attr(this, 'href')).offset().top ) - 51;
+
+	body.animate({ scrollTop: position }, 900);
 });
 
 
@@ -18,7 +17,7 @@ $(window).scroll(function(){
 		$("nav").addClass("nav_fxd");
 	}
 	else{
-		$("nav").removeClass("nav_fxd");		
+		$("nav").removeClass("nav_fxd");
 	}
 });
 
@@ -33,7 +32,7 @@ function animateOnClick(selector , effect , animatDurationMs){
 		$(this).addClass("animated "+ effect + " " + animatDurationMs)
 	});
 
-	//when animation ends 
+	//when animation ends
 	selector.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
 			$(this).removeClass("animated "+ effect + " " + animatDurationMs)
 		});
@@ -44,18 +43,17 @@ function animateOnClick(selector , effect , animatDurationMs){
 // animate Resume_button on half scroll
 $(window).scroll(function(){
 	var aTop = $('.welcome').height();
-	
+
 	if($(this).scrollTop() > aTop/2+aTop-50 &&  $(this).scrollTop() < aTop/2+aTop+50 ){
 		// for debug // alert('header just passed.');
-		
-		$(".welcome button").addClass("animated  rubberBand")	
 
-		//when animation ends 
-		$(".welcome button").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', 
+		$(".welcome button").addClass("animated  rubberBand")
+
+		//when animation ends
+		$(".welcome button").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
 			function(){
 				$(this).removeClass("animated  rubberBand")
 			});
 	} // if end
 
 });// scroll event ends
-
